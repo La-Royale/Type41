9ab33b280aafcda106e96f0f099ffd5cc10f4741
Guillem257 <guillem2525@gmail.com>
2024-11-03 18:24:43 +0100

    gmr_camera
    Implementacion de la camara, cpp i .h. Funciona el movimiento con WASD, el rotar con el raton, F centrar y shift aumenta la velocidad de movimiento

--- sdl2_simple_example/sdl2_simple_example/main.cpp
+++ sdl2_simple_example/sdl2_simple_example/main.cpp
@@ -1,10 +1,11 @@
-#include <GL/glew.h> // Incluye GLEW antes de OpenGL
+#include <GL/glew.h>
 #include <iostream>
 #include <GL/gl.h>
 #include <chrono>
 #include <thread>
 #include <exception>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp> 
 #include <SDL2/SDL_events.h>
 #include <memory>
 #include "MyWindow.h"
@@ -13,6 +14,7 @@
 #include "ModelLoader.h"
 #include "Material.h"
 #include "GameObject.h"
+#include "Camera.h" 
 
 using namespace std;
 
@@ -21,7 +23,7 @@ using u8vec4 = glm::u8vec4;
 using ivec2 = glm::ivec2;
 using vec3 = glm::dvec3;
 
-static const ivec2 WINDOW_SIZE(1600, 900);
+static const ivec2 WINDOW_SIZE(1300, 700);
 static const unsigned int FPS = 60;
 static const auto FRAME_DT = 1.0s / FPS;
 
@@ -30,20 +32,43 @@ static void init_openGL() {
     if (!GLEW_VERSION_3_0) throw exception("OpenGL 3.0 API is not available");
     glEnable(GL_DEPTH_TEST);
     glClearColor(0.5, 0.5, 0.5, 1.0);
+
+    glMatrixMode(GL_PROJECTION);
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), float(WINDOW_SIZE.x) / WINDOW_SIZE.y, 0.1f, 100.0f);
+    glLoadMatrixf(&projection[0][0]);
+    glMatrixMode(GL_MODELVIEW); 
 }
 
-static void display_func(ModelLoader& modelLoader) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    modelLoader.drawModel();
-    glRotatef(0.8f, 1.0f, 1.0f, 0.0f);
-}
-
-static bool processEvents() {
+static bool processEvents(Camera& camera, float deltaTime) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
         case SDL_QUIT:
             return false;
+            break;
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_f) {
+                camera.resetFocus(glm::vec3(0.0f));
+            }
+            else if (event.key.keysym.sym == SDLK_LSHIFT || event.key.keysym.sym == SDLK_RSHIFT) {
+                camera.enableFPSMode(true);
+            }
+            else {
+                camera.processKeyboard(event.key.keysym.sym, deltaTime);
+            }
+            break;
+        case SDL_KEYUP:
+            if (event.key.keysym.sym == SDLK_LSHIFT || event.key.keysym.sym == SDLK_RSHIFT) {
+                camera.enableFPSMode(false);
+            }
+            break;
+        case SDL_MOUSEMOTION:
+            if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_RIGHT)) {
+                camera.processMouseMovement(event.motion.xrel, -event.motion.yrel);
+            }
+            break;
+        case SDL_MOUSEWHEEL:
+            camera.processMouseScroll(event.wheel.y);
             break;
         default:
             ImGui_ImplSDL2_ProcessEvent(&event);
@@ -55,7 +80,7 @@ static bool processEvents() {
 int main(int argc, char** argv) {
     MyWindow window("SDL2 Simple Example", WINDOW_SIZE.x, WINDOW_SIZE.y);
     init_openGL();
-    
+
     std::vector<std::unique_ptr<GameObject>> gameObjects;
 
     auto gameObject1 = std::make_unique<GameObject>();
@@ -64,35 +89,32 @@ int main(int argc, char** argv) {
     Material material;
     material.loadTexture("Baker_house.png");
     gameObject1->setMaterial(material);
-    // gameObject1->setPosition(glm::vec3(0.0f, 0.0f, 0.0f));
-    // gameObject1->setScale(glm::vec3(0.01f, 0.01f, 0.01f));
-    // gameObject1->setRotation(glm::vec3(0.0f, 0.0f, 180.0f));
 
     gameObjects.push_back(std::move(gameObject1));
 
-    // auto gameObject2 = std::make_unique<GameObject>();
-    // gameObject2->loadModel("masterchief.fbx");
-    // gameObjects.push_back(std::move(gameObject2));
-
-    // Añade más GameObjects según sea necesario
-    // auto gameObjectN = std::make_unique<GameObject>();
-    // gameObjectN->loadModel("otro_modelo.fbx");
-    // gameObjects.push_back(std::move(gameObjectN));
-
     WindowEditor editor;
+    Camera camera;
+    float deltaTime = 0.0f;
+    auto lastFrame = hrclock::now();
 
-    while (processEvents()) {
+    while (processEvents(camera, deltaTime)) {
         const auto t0 = hrclock::now();
+        deltaTime = chrono::duration<float>(t0 - lastFrame).count();
+        lastFrame = t0;
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glm::mat4 view = camera.getViewMatrix();
+        glLoadMatrixf(&view[0][0]);
 
         for (auto& gameObject : gameObjects) {
             gameObject->draw();
         }
-        glRotatef(0.8f, 1.0f, 1.0f, 0.0f);
 
-        editor.Render(); // Llama a Render en cada fotograma
+        editor.Render();
 
         window.swapBuffers();
+
         const auto t1 = hrclock::now();
         const auto dt = t1 - t0;
         if (dt < FRAME_DT) this_thread::sleep_for(FRAME_DT - dt);
