5708f2d7deaa8c01a50c812b3e2b0f004cd46128
Guillem257 <guillem2525@gmail.com>
2024-12-28 12:40:16 +0100

    GMR_Selecionobjects

--- sdl2_simple_example/sdl2_simple_example/main.cpp
+++ sdl2_simple_example/sdl2_simple_example/main.cpp
@@ -296,8 +296,8 @@ int main(int argc, char** argv) {
 
             for (auto& gameObject : gameObjects) {
                 if (checkRayIntersection(rayOrigin, rayDirection, *gameObject)) {
-                    // Si la intersección es válida, seleccionamos el objeto
-                    hierarchyPanel.SetSelectedGameObject(gameObject.get());
+                    // Selecionar objetos haciendo clic
+                    //hierarchyPanel.SetSelectedGameObject(gameObject.get());
                     break;
                 }
             }
